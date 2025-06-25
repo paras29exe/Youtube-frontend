ddff165b5324424c0645838b8255e768ec4867fb
paras <dimplesaini5252@gmail.com>
2025-06-26 01:43:04 +0530

    FIxed: FIle path Resolution

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./slices/AuthSlice.js";
 import accountSlice from "./slices/AccountSlice.js";
 import videosSlice from "./slices/VideosSlice.js";
-import likeSubscribeSlice from "./slices/temp.js";
+import likeSubscribeSlice from "./slices/LikeSubscribeSlice.js";
 import commentSlice from "./slices/CommentSlice.js";
 import subscriptionSlice from "./slices/SubscriptionSlice.js";
 import channelSlice from "./slices/ChannelSlice.js";
